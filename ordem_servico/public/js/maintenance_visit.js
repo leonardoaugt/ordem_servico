94299a056e37e4b2d11311e7f2796cf83ba4f7e4
sleonardoaugusto <sleonardoaugusto@gmail.com>
2018-04-24 00:13:16 -0300

    Name field adjustment

--- ordem_servico/public/js/maintenance_visit.js
+++ ordem_servico/public/js/maintenance_visit.js
@@ -131,8 +131,8 @@ frappe.ui.form.on("Maintenance Visit Purpose", {
 				callback: function (r) {
 					data = r.message;
 					idx = (d.idx - 1);
-					cur_frm.doc.purposes[idx].agendado_para= data['employee_name'];
-					cur_frm.refresh_field("agendado_para");
+					cur_frm.doc.purposes[idx].agendado_para_name = data['employee_name'];
+					cur_frm.refresh_field("agendado_para_name");
 				}
 			});
 		}
