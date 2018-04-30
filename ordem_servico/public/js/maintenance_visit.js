2676b4988668d6777b8ebbe47e45fad63294c69e
sleonardoaugusto <sleonardoaugusto@gmail.com>
2018-04-30 16:13:23 -0300

    debugging resource not available

--- ordem_servico/public/js/maintenance_visit.js
+++ ordem_servico/public/js/maintenance_visit.js
@@ -1,253 +1,248 @@
-frappe.ui.form.on("Maintenance Visit", {
+// frappe.ui.form.on("Maintenance Visit", {
 
-	after_save: function (frm) {
+// 	after_save: function (frm) {
 
-		// Rename Quotation
+// 		// Rename Quotation
 
-		frappe.call({
-			method: "ordem_servico.ordem_servico.ordem_servico.rename_quotation",
-			args: {
-				doc_maint: frm.doc.name,
-			},
-			callback: function (r) {
-				cur_frm.__unsaved = 1;
-			}
-		});
+// 		frappe.call({
+// 			method: "ordem_servico.ordem_servico.ordem_servico.rename_quotation",
+// 			args: {
+// 				doc_maint: frm.doc.name,
+// 			},
+// 			callback: function (r) {
+// 				cur_frm.__unsaved = 1;
+// 			}
+// 		});
 
-		// Create event based on "agendado_para"
+// 		// Create event based on "agendado_para"
 
-		$.each(frm.doc.purposes || [], function (i, d) {
-			frappe.call({
-				method: "ordem_servico.ordem_servico.ordem_servico.make_event",
-				args: {
-					maint_name: frm.doc.name,
-					os_name: d.os,
-					customer: frm.doc.customer,
-					employee: d.agendado_para,
-				},
-				callback: function (r) {
-					data = r.message;
-					console.log(data);
-					d.agenda = data['name'];
-				}
-			});
-		});
-		frm.reload_doc();
-		frm.refresh_field('purposes');
-	},
+// 		frappe.call({
+// 			method: "ordem_servico.ordem_servico.ordem_servico.make_event",
+// 			args: {
+// 				doc_name: frm.doc.name,
+// 			},
+// 			callback: function (r) {
+// 				data = r.message;
+// 				console.log(data);
+// 				d.agenda = data['name'];
+// 			}
+// 		});
+// 		frm.reload_doc();
+// 		frm.refresh_field('purposes');
+// 	},
 
-	customer: function (frm) {
+// 	customer: function (frm) {
 
-		// Clear purposes when change customer
-		if (frm.doc.purposes) {
-			frm.doc.purposes = [];
-			frm.add_child('purposes');
-		}
-	},
+// 		// Clear purposes when change customer
+// 		if (frm.doc.purposes) {
+// 			frm.doc.purposes = [];
+// 			frm.add_child('purposes');
+// 		}
+// 	},
 
-	local_manutencao: function (frm) {
+// 	local_manutencao: function (frm) {
 
-		// Set serie_number
-		frm.fields_dict.purposes.grid.get_field('numero_serie').get_query = function () {
-			return {
-				filters: {
-					"parent": cur_frm.doc.customer
-				}
-			}
-		}
+// 		// Set serie_number
+// 		frm.fields_dict.purposes.grid.get_field('numero_serie').get_query = function () {
+// 			return {
+// 				filters: {
+// 					"parent": cur_frm.doc.customer
+// 				}
+// 			}
+// 		}
 
-		frm.fields_dict.purposes.grid.get_field('agendado_para').get_query = function () {
-			return {
-				filters: {
-					"department": ["in", ["Diretoria", "Assistência Técnica"]]
-				}
-			}
-		}
+// 		frm.fields_dict.purposes.grid.get_field('agendado_para').get_query = function () {
+// 			return {
+// 				filters: {
+// 					"department": ["in", ["Diretoria", "Assistência Técnica"]]
+// 				}
+// 			}
+// 		}
 
-		frm.fields_dict.purposes.grid.get_field('agendado_para2').get_query = function () {
-			return {
-				filters: {
-					"department": ["in", ["Diretoria", "Assistência Técnica"]]
-				}
-			}
-		}
+// 		frm.fields_dict.purposes.grid.get_field('agendado_para2').get_query = function () {
+// 			return {
+// 				filters: {
+// 					"department": ["in", ["Diretoria", "Assistência Técnica"]]
+// 				}
+// 			}
+// 		}
 
-		frm.fields_dict.purposes.grid.get_field('agendado_por').get_query = function () {
-			return {
-				filters: {
-					"department": ["in", ["Diretoria", "Vendas"]]
-				}
-			}
-		}
+// 		frm.fields_dict.purposes.grid.get_field('agendado_por').get_query = function () {
+// 			return {
+// 				filters: {
+// 					"department": ["in", ["Diretoria", "Vendas"]]
+// 				}
+// 			}
+// 		}
 
-		frm.fields_dict.purposes.grid.get_field('agendado_por2').get_query = function () {
-			return {
-				filters: {
-					"department": ["in", ["Diretoria", "Vendas"]]
-				}
-			}
-		}
-	}
+// 		frm.fields_dict.purposes.grid.get_field('agendado_por2').get_query = function () {
+// 			return {
+// 				filters: {
+// 					"department": ["in", ["Diretoria", "Vendas"]]
+// 				}
+// 			}
+// 		}
+// 	}
 
-});
+// });
 
-frappe.ui.form.on("Maintenance Visit Purpose", {
+// frappe.ui.form.on("Maintenance Visit Purpose", {
 
-	// Create quotation doc
+// 	// Create quotation doc
 
-	orcamento: function (frm, cdt, cdn) {
-		if (!frm.doc.__unsaved) {
-			d = locals[cdt][cdn]
-			if (d.garantia == 0) {
-				if (frm.doc.local_manutencao == "Interno") {
-					// Take index datas
-					d = locals[cdt][cdn];
-					frappe.call({
-						method: "ordem_servico.ordem_servico.ordem_servico.new_quotation",
-						args: {
-							doc_maint: frm.doc.name,
-							purposes_os: d.os,
-						},
-						callback: function (r) {
-							var doc = r.message;
-							frappe.set_route("Form", "Quotation", doc.name);
-						}
-					});
-				} else { frappe.throw("Local de Manutenção deve ser Interno!") }
-			} else { frappe.throw("Equipamento na garantia!") }
-		} else { frappe.throw("Salve o documento primeiro!") }
-	},
+// 	orcamento: function (frm, cdt, cdn) {
+// 		if (!frm.doc.__unsaved) {
+// 			d = locals[cdt][cdn]
+// 			if (d.garantia == 0) {
+// 				if (frm.doc.local_manutencao == "Interno") {
+// 					// Take index datas
+// 					d = locals[cdt][cdn];
+// 					frappe.call({
+// 						method: "ordem_servico.ordem_servico.ordem_servico.new_quotation",
+// 						args: {
+// 							doc_maint: frm.doc.name,
+// 							purposes_os: d.os,
+// 						},
+// 						callback: function (r) {
+// 							var doc = r.message;
+// 							frappe.set_route("Form", "Quotation", doc.name);
+// 						}
+// 					});
+// 				} else { frappe.throw("Local de Manutenção deve ser Interno!") }
+// 			} else { frappe.throw("Equipamento na garantia!") }
+// 		} else { frappe.throw("Salve o documento primeiro!") }
+// 	},
 
-	// Get serie number
+// 	// Get serie number
 
-	numero_serie: function (frm, cdt, cdn) {
-		d = locals[cdt][cdn];
-		if (d.numero_serie) {
-			frappe.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Materiais",
-					filters: {
-						numero_serie: d.numero_serie,
-					},
-					fieldname: ["modelo", "descricao", "tag"]
-				},
-				callback: function (r) {
-					data = r.message;
-					idx = (d.idx - 1);
-					cur_frm.doc.purposes[idx].item_name = data['descricao'];
-					cur_frm.doc.purposes[idx].modelo_equipamento = data['modelo'];
-					cur_frm.doc.purposes[idx].tag = data['tag'];
-					cur_frm.refresh_field("purposes");
-				}
-			});
-		}
-	},
+// 	numero_serie: function (frm, cdt, cdn) {
+// 		d = locals[cdt][cdn];
+// 		if (d.numero_serie) {
+// 			frappe.call({
+// 				method: "frappe.client.get_value",
+// 				args: {
+// 					doctype: "Materiais",
+// 					filters: {
+// 						numero_serie: d.numero_serie,
+// 					},
+// 					fieldname: ["modelo", "descricao", "tag"]
+// 				},
+// 				callback: function (r) {
+// 					data = r.message;
+// 					idx = (d.idx - 1);
+// 					cur_frm.doc.purposes[idx].item_name = data['descricao'];
+// 					cur_frm.doc.purposes[idx].modelo_equipamento = data['modelo'];
+// 					cur_frm.doc.purposes[idx].tag = data['tag'];
+// 					cur_frm.refresh_field("purposes");
+// 				}
+// 			});
+// 		}
+// 	},
 
-	// Get employee_names
+// 	// Get employee_names
 
-	agendado_para: function (frm, cdt, cdn) {
-		d = locals[cdt][cdn];
-		if (d.agendado_para) {
-			frappe.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Employee",
-					filters: {
-						name: d.agendado_para,
-					},
-					fieldname: "employee_name"
-				},
-				callback: function (r) {
-					data = r.message;
-					idx = (d.idx - 1);
-					cur_frm.doc.purposes[idx].agendado_para_name = data['employee_name'];
-					cur_frm.refresh_field("purposes");
-				}
-			});
-		}
-		else {
-			d.agendado_para_name = "";
-			cur_frm.refresh_field("purposes");
-		}
-	},
+// 	agendado_para: function (frm, cdt, cdn) {
+// 		d = locals[cdt][cdn];
+// 		if (d.agendado_para) {
+// 			frappe.call({
+// 				method: "frappe.client.get_value",
+// 				args: {
+// 					doctype: "Employee",
+// 					filters: {
+// 						name: d.agendado_para,
+// 					},
+// 					fieldname: "employee_name"
+// 				},
+// 				callback: function (r) {
+// 					data = r.message;
+// 					idx = (d.idx - 1);
+// 					cur_frm.doc.purposes[idx].agendado_para_name = data['employee_name'];
+// 					cur_frm.refresh_field("purposes");
+// 				}
+// 			});
+// 		}
+// 		else {
+// 			d.agendado_para_name = "";
+// 			cur_frm.refresh_field("purposes");
+// 		}
+// 	},
 
-	agendado_por: function (frm, cdt, cdn) {
-		d = locals[cdt][cdn];
-		if (d.agendado_por) {
-			frappe.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Employee",
-					filters: {
-						name: d.agendado_por,
-					},
-					fieldname: "employee_name"
-				},
-				callback: function (r) {
-					data = r.message;
-					idx = (d.idx - 1);
-					cur_frm.doc.purposes[idx].agendado_por_name = data['employee_name'];
-					cur_frm.refresh_field("purposes");
-				}
-			});
-		}
-		else {
-			d.agendado_por_name = "";
-			cur_frm.refresh_field("purposes");
-		}
-	},
+// 	agendado_por: function (frm, cdt, cdn) {
+// 		d = locals[cdt][cdn];
+// 		if (d.agendado_por) {
+// 			frappe.call({
+// 				method: "frappe.client.get_value",
+// 				args: {
+// 					doctype: "Employee",
+// 					filters: {
+// 						name: d.agendado_por,
+// 					},
+// 					fieldname: "employee_name"
+// 				},
+// 				callback: function (r) {
+// 					data = r.message;
+// 					idx = (d.idx - 1);
+// 					cur_frm.doc.purposes[idx].agendado_por_name = data['employee_name'];
+// 					cur_frm.refresh_field("purposes");
+// 				}
+// 			});
+// 		}
+// 		else {
+// 			d.agendado_por_name = "";
+// 			cur_frm.refresh_field("purposes");
+// 		}
+// 	},
 
-	agendado_para2: function (frm, cdt, cdn) {
-		d = locals[cdt][cdn];
-		if (d.agendado_para2) {
-			frappe.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Employee",
-					filters: {
-						name: d.agendado_para2,
-					},
-					fieldname: "employee_name"
-				},
-				callback: function (r) {
-					data = r.message;
-					idx = (d.idx - 1);
-					cur_frm.doc.purposes[idx].agendado_para_name2 = data['employee_name'];
-					cur_frm.refresh_field("purposes");
-				}
-			});
-		}
-		else {
-			d.agendado_para_name2 = "";
-			cur_frm.refresh_field("purposes");
-		}
-	},
+// 	agendado_para2: function (frm, cdt, cdn) {
+// 		d = locals[cdt][cdn];
+// 		if (d.agendado_para2) {
+// 			frappe.call({
+// 				method: "frappe.client.get_value",
+// 				args: {
+// 					doctype: "Employee",
+// 					filters: {
+// 						name: d.agendado_para2,
+// 					},
+// 					fieldname: "employee_name"
+// 				},
+// 				callback: function (r) {
+// 					data = r.message;
+// 					idx = (d.idx - 1);
+// 					cur_frm.doc.purposes[idx].agendado_para_name2 = data['employee_name'];
+// 					cur_frm.refresh_field("purposes");
+// 				}
+// 			});
+// 		}
+// 		else {
+// 			d.agendado_para_name2 = "";
+// 			cur_frm.refresh_field("purposes");
+// 		}
+// 	},
 
-	agendado_por2: function (frm, cdt, cdn) {
-		d = locals[cdt][cdn];
-		if (d.agendado_por) {
-			frappe.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Employee",
-					filters: {
-						name: d.agendado_por,
-					},
-					fieldname: "employee_name"
-				},
-				callback: function (r) {
-					data = r.message;
-					idx = (d.idx - 1);
-					cur_frm.doc.purposes[idx].agendado_por_name2 = data['employee_name'];
-					cur_frm.refresh_field("purposes");
-				}
-			});
-		}
-		else {
-			d.agendado_por_name2 = "";
-			cur_frm.refresh_field("purposes");
-		}
-	},
+// 	agendado_por2: function (frm, cdt, cdn) {
+// 		d = locals[cdt][cdn];
+// 		if (d.agendado_por) {
+// 			frappe.call({
+// 				method: "frappe.client.get_value",
+// 				args: {
+// 					doctype: "Employee",
+// 					filters: {
+// 						name: d.agendado_por,
+// 					},
+// 					fieldname: "employee_name"
+// 				},
+// 				callback: function (r) {
+// 					data = r.message;
+// 					idx = (d.idx - 1);
+// 					cur_frm.doc.purposes[idx].agendado_por_name2 = data['employee_name'];
+// 					cur_frm.refresh_field("purposes");
+// 				}
+// 			});
+// 		}
+// 		else {
+// 			d.agendado_por_name2 = "";
+// 			cur_frm.refresh_field("purposes");
+// 		}
+// 	},
 
-});
+// });
