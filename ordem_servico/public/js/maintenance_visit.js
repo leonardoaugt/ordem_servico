ec1e6274bae672abff63d50de0d2f24dffa09658
sleonardoaugusto <sleonardoaugusto@gmail.com>
2018-05-02 10:01:04 -0300

    debugging numero_serie trigger

--- ordem_servico/public/js/maintenance_visit.js
+++ ordem_servico/public/js/maintenance_visit.js
@@ -123,7 +123,7 @@ frappe.ui.form.on("Maintenance Visit Purpose", {
 				args: {
 					doctype: "Materiais",
 					filters: {
-						parent: doc.name,
+						parent: d.name,
 						numero_serie: d.numero_serie,
 					},
 					fieldname: ["modelo", "descricao", "tag"]
