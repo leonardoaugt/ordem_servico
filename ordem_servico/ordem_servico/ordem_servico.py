1174af66525607e7e2533263eddfb0f20deef0d1
leonardoaugt <leonardoaugt@hotmail.com>
2018-05-22 03:15:57 -0300

    Create first and second event

--- ordem_servico/ordem_servico/ordem_servico.py
+++ ordem_servico/ordem_servico/ordem_servico.py
@@ -63,9 +63,9 @@ def make_event(doc_name):
     purposes = maint.purposes
 
     for row in purposes:
-        # if not agenda value on purposes_idx and has agendado_para
+
+        # Create first event
         if (not row.evento_link and row.agendado_para):
-            # create event document
             event = frappe.new_doc("Event")
             event.subject = row.os
             datetime_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:00")
@@ -77,7 +77,23 @@ def make_event(doc_name):
             event.owner = frappe.db.get_value("Employee", row.agendado_para, "user_id")
             event.save()
 
-            # set event doc name to purposes row
+            row.data_agendamento_orcamento = datetime.datetime.today().strftime('%Y-%m-%d')
+            row.evento_link = event.name
+            row.save()
+
+        # Create second event
+        elif (not row.evento_link2 and row.agendado_para2):
+            event = frappe.new_doc("Event")
+            event.subject = row.os
+            datetime_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:00")
+            event.starts_on = datetime_now
+            event.ends_on = datetime_now
+            event.manutencao = maint.name
+            event.ordem_servico = row.os
+            event.ref_type = "Maintenance Visit Purpose"
+            event.owner = frappe.db.get_value("Employee", row.agendado_para2, "user_id")
+            event.save()
+
             row.data_agendamento_orcamento = datetime.datetime.today().strftime('%Y-%m-%d')
             row.evento_link = event.name
             row.save()
