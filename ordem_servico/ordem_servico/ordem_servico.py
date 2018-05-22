391d9fe34ff7bf62a0c7c2d91bd4fab930e4d274
leonardoaugt <leonardoaugt@hotmail.com>
2018-05-22 01:35:12 -0300

    set as_dict=True to data return

--- ordem_servico/ordem_servico/ordem_servico.py
+++ ordem_servico/ordem_servico/ordem_servico.py
@@ -110,5 +110,5 @@ def custom_get_value(doctype, fieldname, filters=None, as_dict=True, debug=False
 @frappe.whitelist()
 def get_tempo_orcamento_conserto(equipamento):
     familia = frappe.db.get_value('Equipamentos', {'name': equipamento}, ['familia'])
-    data = frappe.db.get_value('Familias de Equipamentos', {'name': familia}, ['tempo_orcamento', 'tempo_conserto'])
+    data = frappe.db.get_value('Familias de Equipamentos', {'name': familia}, ['tempo_orcamento', 'tempo_conserto'], as_dict=True)
     return data
