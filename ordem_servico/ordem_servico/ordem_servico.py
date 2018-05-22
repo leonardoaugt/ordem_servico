f394a2f1b09fb7dbc5b2fc364ed0cf6d7bbeef0e
leonardoaugt <leonardoaugt@hotmail.com>
2018-05-22 00:54:08 -0300

    set args method properly

--- ordem_servico/ordem_servico/ordem_servico.py
+++ ordem_servico/ordem_servico/ordem_servico.py
@@ -109,6 +109,6 @@ def custom_get_value(doctype, fieldname, filters=None, as_dict=True, debug=False
 
 @frappe.whitelist()
 def get_tempo_orcamento(equipamento):
-    familia = frappe.db.get_value('tabEquipamentos', {'name': equipamento})
+    familia = frappe.db.get_value('tabEquipamentos', {'name': equipamento}, ['familia'])
     tempo_orcamento = "SELECT tempo_orcamento FROM `tabFamilias de Equipamentos` WHERE name='{}'".format(familia)
     return tempo_orcamento
