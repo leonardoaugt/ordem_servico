6ec314fd38a431bd8ae27960d856b19ab3fd34a4
leonardoaugt <leonardoaugt@hotmail.com>
2018-05-29 14:23:05 -0300

    frappe.call fixed

--- ordem_servico/public/js/event_calendar.js
+++ ordem_servico/public/js/event_calendar.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Event', {
     iniciar_cronometro: function () {
-        frape.call({
+        frappe.call({
             method: 'ordem_servico.ordem_servico.event_document.start_maintenance',
             args: {
                 docname: frm.doc.name,
