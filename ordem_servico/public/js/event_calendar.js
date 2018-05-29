0f39de7d9af695b4233a707e2c79c512cf1fe4f7
leonardoaugt <leonardoaugt@hotmail.com>
2018-05-29 14:20:07 -0300

    trigger name adjustment

--- ordem_servico/public/js/event_calendar.js
+++ ordem_servico/public/js/event_calendar.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on('Event', {
-    iniciar_manutencao: function () {
+    iniciar_cronometro: function () {
         frape.call({
             method: 'ordem_servico.ordem_servico.event_document.start_maintenance',
             args: {
