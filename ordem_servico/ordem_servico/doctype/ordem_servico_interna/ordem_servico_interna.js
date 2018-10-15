707361096cc03897a4c5934ea3e589bae98b3081
leonardo augusto <sleonardoaugusto@gmail.com>
2018-10-15 16:22:01 -0300

    Fields get_query

--- ordem_servico/ordem_servico/doctype/ordem_servico_interna/ordem_servico_interna.js
+++ ordem_servico/ordem_servico/doctype/ordem_servico_interna/ordem_servico_interna.js
@@ -2,7 +2,56 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Ordem Servico Interna', {
-	refresh: function(frm) {
 
-	}
+	customer: function(frm) {
+		
+		frm.fields_dict.serie_number.get_query = function () {
+		   console.log('testing');
+		   return {
+			   filters: {
+				   'parent': cur_frm.doc.customer
+			   }
+		   }
+	   }
+
+		frm.fields_dict.initial_scheduled_to.get_query = function () {
+		   return {
+			   filters: {
+				   'department': ['in', ['Diretoria', 'Assistência Técnica']]
+			   }
+		   }
+	   }
+
+		frm.fields_dict.initial_scheduled_by.get_query = function () {
+		   return {
+			   filters: {
+				   'department': ['in', ['Diretoria', 'Vendas']]
+			   }
+		   }
+	   }
+
+		frm.fields_dict.technical_person.get_query = function () {
+		   return {
+			   filters: {
+				   'department': ['in', ['Diretoria', 'Assistência Técnica']]
+			   }
+		   }
+	   }
+
+		frm.fields_dict.final_scheduled_to.get_query = function () {
+		   return {
+			   filters: {
+				   'department': ['in', ['Diretoria', 'Vendas']]
+			   }
+		   }
+	   }
+
+		frm.fields_dict.final_scheduled_by.get_query = function () {
+		   return {
+			   filters: {
+				   'department': ['in', ['Diretoria', 'Assistência Técnica']]
+			   }
+		   }
+	   }
+   }
 });
