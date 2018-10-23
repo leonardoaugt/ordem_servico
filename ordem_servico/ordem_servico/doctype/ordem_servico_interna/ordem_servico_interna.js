0ca4987e49150f274c14bc7139196a26d10acf39
leonardo augusto <sleonardoaugusto@gmail.com>
2018-10-23 08:49:49 -0300

    Moduling querysets

--- ordem_servico/ordem_servico/doctype/ordem_servico_interna/ordem_servico_interna.js
+++ ordem_servico/ordem_servico/doctype/ordem_servico_interna/ordem_servico_interna.js
@@ -1,101 +1,36 @@
 // Copyright (c) 2018, laugusto and contributors
 // For license information, please see license.txt
 
+{% include "ordem_servico/public/js/ordem_servico.js" %}
+
 frappe.ui.form.on('Ordem Servico Interna', {
 
-	customer: function (frm) {
-
-		frm.fields_dict.serie_number.get_query = function () {
-			return {
-				filters: {
-					'parent': cur_frm.doc.customer
-				}
-			}
+	schedule_quotation_event: function () {
+		
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
 		}
-
-		frm.fields_dict.initial_scheduled_to.get_query = function () {
-			return {
-				filters: {
-					'department': ['in', ['Diretoria', 'Assistência Técnica']]
-				}
-			}
-		}
-
-		frm.fields_dict.initial_scheduled_by.get_query = function () {
-			return {
-				filters: {
-					'department': ['in', ['Diretoria', 'Vendas']]
-				}
-			}
-		}
-
-		frm.fields_dict.technical_person.get_query = function () {
-			return {
-				filters: {
-					'department': ['in', ['Diretoria', 'Assistência Técnica']]
-				}
-			}
-		}
-
-		frm.fields_dict.final_scheduled_to.get_query = function () {
-			return {
-				filters: {
-					'department': ['in', ['Diretoria', 'Vendas']]
-				}
-			}
-		}
-
-		frm.fields_dict.final_scheduled_by.get_query = function () {
-			return {
-				filters: {
-					'department': ['in', ['Diretoria', 'Assistência Técnica']]
-				}
-			}
-		}
+		frappe.call({
+			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.make_event',
+			args: {
+				doctype: frm.doc.doctype,
+				docname: frm.doc.name,
+				start_date: frm.doc.quotation_schedule_date,
+				start_time: frm.doc.quotation_schedule_time,
+				work_time: frm.doc.quotation_time,
+				trigger: 'quotation',
+			},
+		});
+		frm.reload_doc();
+		show_alert('Orçamento agendado.');
 	},
 
-	serie_number: function (frm) {
 
-		if (frm.doc.serie_number) {
-			frappe.call({
-				method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.get_repair_and_quotation_times',
-				args: {
-					equipment: frm.doc.equipment_model,
-				},
-				callback: function (r) {
-					data = r.message;
-					frm.doc.quotation_time = data['quotation_time'];
-					frm.doc.repair_time = data['repair_time'];
-					frm.refresh_field('quotation_time');
-					frm.refresh_field('repair_time');
-				}
-			});
+	start_quotation: function () {
+
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
 		}
-	},
-
-	schedule_quotation_event: function (frm) {
-
-		if (frm.doc.__unsaved) {
-			frappe.throw('Favor salvar o documento.');
-		} else {
-			frappe.call({
-				method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.make_event',
-				args: {
-					doctype: frm.doc.doctype,
-					docname: frm.doc.name,
-					start_date: frm.doc.quotation_schedule_date,
-					start_time: frm.doc.quotation_schedule_time,
-					work_time: frm.doc.quotation_time,
-					trigger: 'quotation',
-				},
-			});
-			frm.reload_doc();
-			show_alert('Orçamento agendado.');
-		}
-	},
-
-	start_quotation: function (frm) {
-
 		frappe.call({
 			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.get_time_now',
 			args: {
@@ -108,8 +43,11 @@ frappe.ui.form.on('Ordem Servico Interna', {
 		frm.reload_doc();
 	},
 
-	end_quotation: function (frm) {
+	end_quotation: function () {
 
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
+		}
 		frappe.call({
 			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.get_time_now',
 			args: {
@@ -122,47 +60,48 @@ frappe.ui.form.on('Ordem Servico Interna', {
 		frm.reload_doc();
 	},
 
-	create_quotation: function (frm) {
+	create_quotation: function () {
 
-		if (frm.doc.__unsaved) {
-			frappe.throw('Favor salvar o documento.');
-		} else {
-			frappe.call({
-				method: 'ordem_servico.ordem_servico.utils.make_quotation',
-				args: {
-					doctype: frm.doc.doctype,
-					docname: frm.doc.name,
-					local: 'Interno',
-				},
-			});
-			frm.reload_doc();
-			show_alert('Orçamento gerado.');
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
 		}
+		frappe.call({
+			method: 'ordem_servico.ordem_servico.utils.make_quotation',
+			args: {
+				doctype: frm.doc.doctype,
+				docname: frm.doc.name,
+				local: 'Interno',
+			},
+		});
+		frm.reload_doc();
+		show_alert('Orçamento gerado.');
 	},
 
-	schedule_repair_event: function (frm) {
+	schedule_repair_event: function () {
 
-		if (frm.doc.__unsaved) {
-			frappe.throw('Favor salvar o documento.');
-		} else {
-			frappe.call({
-				method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.make_event',
-				args: {
-					doctype: frm.doc.doctype,
-					docname: frm.doc.name,
-					start_date: frm.doc.repair_schedule_date,
-					start_time: frm.doc.repair_schedule_time,
-					work_time: frm.doc.repair_time,
-					trigger: 'repair',
-				},
-			});
-			frm.reload_doc();
-			show_alert('Conserto agendado.');
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
 		}
+		frappe.call({
+			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.make_event',
+			args: {
+				doctype: frm.doc.doctype,
+				docname: frm.doc.name,
+				start_date: frm.doc.repair_schedule_date,
+				start_time: frm.doc.repair_schedule_time,
+				work_time: frm.doc.repair_time,
+				trigger: 'repair',
+			},
+		});
+		frm.reload_doc();
+		show_alert('Conserto agendado.');
 	},
 
-	start_repair: function (frm) {
+	start_repair: function () {
 
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
+		}
 		frappe.call({
 			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.get_time_now',
 			args: {
@@ -175,8 +114,11 @@ frappe.ui.form.on('Ordem Servico Interna', {
 		show_alert('Conserto iniciado.');
 	},
 
-	end_repair: function (frm) {
+	end_repair: function () {
 
+		if (cur_frm.doc.__unsaved) {
+			frappe.throw('Favor salvar documento!');
+		}
 		frappe.call({
 			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.get_time_now',
 			args: {
---
+++ ordem_servico/public/js/ordem_servico.js
@@ -0,0 +1,79 @@
+// Copyright (c) 2018, laugusto and contributors
+// For license information, please see license.txt
+
+
+frappe.ui.form.on(cur_frm.doctype, {
+
+	onload: function () {
+
+		this.cur_frm.fields_dict.serie_number.get_query = function () {
+			return {
+				filters: {
+					'parent': cur_frm.doc.customer
+				}
+			}
+		}
+
+		this.cur_frm.fields_dict.initial_scheduled_to.get_query = function () {
+			return {
+				filters: {
+					'department': ['in', ['Diretoria', 'Assistência Técnica']]
+				}
+			}
+		}
+
+		this.cur_frm.fields_dict.initial_scheduled_by.get_query = function () {
+			return {
+				filters: {
+					'department': ['in', ['Diretoria', 'Vendas']]
+				}
+			}
+		}
+
+		this.cur_frm.fields_dict.technical_person.get_query = function () {
+			return {
+				filters: {
+					'department': ['in', ['Diretoria', 'Assistência Técnica']]
+				}
+			}
+		}
+
+		this.cur_frm.fields_dict.final_scheduled_to.get_query = function () {
+			return {
+				filters: {
+					'department': ['in', ['Diretoria', 'Vendas']]
+				}
+			}
+		}
+
+		this.cur_frm.fields_dict.final_scheduled_by.get_query = function () {
+			return {
+				filters: {
+					'department': ['in', ['Diretoria', 'Assistência Técnica']]
+				}
+			}
+		}
+	},
+
+	serie_number: function (frm) {
+
+		if (frm.doc.serie_number) {
+			frappe.call({
+				method: 'ordem_servico.ordem_servico.doctype.ordem_servico_interna.ordem_servico_interna.get_repair_and_quotation_times',
+				args: {
+					equipment: frm.doc.equipment_model,
+				},
+				callback: function (r) {
+					data = r.message;
+					frm.doc.quotation_time = data['quotation_time'];
+					frm.doc.repair_time = data['repair_time'];
+					frm.refresh_field('quotation_time');
+					frm.refresh_field('repair_time');
+				}
+			});
+		}
+	},
+
+	
+
+});
