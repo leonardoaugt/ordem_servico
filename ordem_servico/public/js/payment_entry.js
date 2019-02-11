ccce6810a3800d162acfd244c5abfb249b0e9b3c
Leonardo <sleonardoaugusto@gmail.com>
2019-02-11 01:30:48 -0200

    Set Payment Entry OS History

--- ordem_servico/public/js/payment_entry.js
+++ ordem_servico/public/js/payment_entry.js
@@ -1,16 +1,16 @@
 frappe.ui.form.on('Payment Entry', {
 
-    onload: function (frm) {
+    onload_post_render: function (frm) {
         if (frm.doc.__islocal) {
-            // Get previous doc route
-            var prev_route = frappe.get_prev_route();
-            var prev_doctype = prev_route[1];
-            var prev_docname = prev_route[2];
-            var prev_doc = frappe.get_doc(prev_doctype[1], prev_docname[2]);
+            // Get document previous route
+            prev_route = frappe.get_prev_route();
+            prev_doctype = prev_route[1];
+            prev_docname = prev_route[2];
+            prev_doc = frappe.get_doc(prev_doctype, prev_docname);
             console.log(prev_doc);
 
             // Check if is a Sales Order and has os_interna_link
-            if (prev_doc.doctype == 'Sales Order' && prev_doc.os_interna_link) {
+            if (prev_doc.doctype == 'Sales Order' || prev_doc.doctype == 'Quotation' && prev_doc.os_interna_link) {
                 frm.doc.os_interna_link = prev_doc.os_interna_link;
                 frm.doc.local_manutencao = prev_doc.local_manutencao;
                 refresh_field('os_interna_link');
@@ -26,7 +26,7 @@ frappe.ui.form.on('Payment Entry', {
                 method: 'ordem_servico.ordem_servico.utils.set_payment_entry_history',
                 args: {
                     source_docname: frm.doc.name,
-                    source_transaction_date: frm.doc.transaction_date,
+                    source_transaction_date: frm.doc.posting_date,
                     target_docname: frm.doc.os_interna_link,
                 }
             });
