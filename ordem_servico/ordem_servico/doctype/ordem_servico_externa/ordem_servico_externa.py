ce85196b0d8d19e2f2cb94fb160dfae9c953e36a
Leonardo <sleonardoaugusto@gmail.com>
2019-03-09 16:54:51 -0300

    Making OS Externa through Sales Order

--- ordem_servico/ordem_servico/doctype/ordem_servico_externa/ordem_servico_externa.py
+++ ordem_servico/ordem_servico/doctype/ordem_servico_externa/ordem_servico_externa.py
@@ -5,9 +5,24 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from ...utils import time_now
+import datetime
 
 
 class OrdemServicoExterna(Document):
 	pass
+
+@frappe.whitelist()
+def make_os_externa(docname):
+    sales_order = frappe.get_doc('Sales Order', docname)
+    os_externa = frappe.new_doc('Ordem Servico Externa')
+    os_externa.customer = sales_order.customer
+    os_externa.transaction_date = datetime.datetime.now().date()
+    os_externa.customer_address = sales_order.customer_address
+    os_externa.address_display = sales_order.address_display
+    os_externa.contact_person = sales_order.contact_person
+    os_externa.contact_display = sales_order.contact_display
+    os_externa.contact_mobile = sales_order.contact_mobile
+    return os_externa
 
 
--- ordem_servico/public/js/sales_order.js
+++ ordem_servico/public/js/sales_order.js
@@ -1,5 +1,19 @@
 frappe.ui.form.on('Sales Order', {
 
+    setup: function (frm) {
+        frm.doc.custom_make_buttons = {
+            'Ordem Serviço Externa': 'Make OS Externa'
+        }
+    },
+
+    refresh: function (frm) {
+        if (frm.doc.docstatus == 1
+            && frm.doc.local_manutencao == 'Interna') {
+            frm.add_custom_button(__('Ordem Serviço Externa'),
+                frm.cscript['Make OS Externa'], __("Make"));
+        }
+    },
+
     after_save: function (frm) {
         //Set Sales Order on OS History section
         if (frm.doc.os_interna_link) {
@@ -14,3 +28,18 @@ frappe.ui.form.on('Sales Order', {
         }
     }
 });
+
+cur_frm.cscript['Make OS Externa'] = function () {
+
+    frappe.call({
+        method: "ordem_servico.ordem_servico.doctype.ordem_servico_externa.ordem_servico_externa.make_os_externa",
+        args: {
+            docname: cur_frm.doc.name,
+        },
+        callback: function (r) {
+            frappe.model.sync(r.message);
+            console.log(r.message);
+            frappe.set_route('Form', r.message.doctype, r.message.name);
+        }
+    })
+}
