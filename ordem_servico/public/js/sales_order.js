024b3fe0a0ec1a53719a57d09d2a737063d0e62f
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-05-03 08:30:55 -0300

    Validate to add Make OS Externa button

--- ordem_servico/public/js/sales_order.js
+++ ordem_servico/public/js/sales_order.js
@@ -1,4 +1,23 @@
+status = {
+    EXTERNA: 'Externo',
+};
+externa = status.EXTERNA;
+
 frappe.ui.form.on('Sales Order', {
+
+    setup: function (frm) {
+        frm.custom_make_buttons = {
+            'Ordem Serviço Externa': 'OS Externa',
+        }
+    },
+
+    onload: function (frm) {
+        if (frm.events.is_externa(frm)) {
+            cur_frm.add_custom_button(__('OS Externa'),
+                () => make_os(frm),
+                __("Make"));
+        }
+    },
 
     after_save: function (frm) {
         //Set Sales Order on OS History section
@@ -12,5 +31,28 @@ frappe.ui.form.on('Sales Order', {
                 }
             });
         }
+    },
+
+    make_os: function (frm) {
+        frappe.call({
+            method: 'ordem_servico.ordem_servico.doctype.ordem_servico_externa.ordem_servico_externa.make_os_externa',
+            args: {
+                docname: frm.doc.name,
+            },
+        })
+    },
+
+    is_externa: function (frm) {
+        if (frm.doc.local_manutencao == externa &&
+            frm.doc.docstatus == 1 &&
+            (!frm.doc.os_interna_link ||
+                frm.doc.os_interna_link == undefined)
+        ) {
+            valid = true;
+        } else {
+            valid = false;
+        }
+
+        return valid;
     }
 });
