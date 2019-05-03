88e44076397c84e671d8a2478ae87243140be5c6
Ubuntu <sleonardoaugusto@gmail.com>
2019-05-03 02:36:03 +0000

    Manutencao Externa DocType added

---
+++ ordem_servico/ordem_servico/doctype/manutencao_externa/manutencao_externa.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, laugusto and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Manutencao Externa', {
+	refresh: function(frm) {
+
+	}
+});
