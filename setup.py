8d73684deeb9b20a4d5a8f1186ff11d052eb573a
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-05-04 14:33:37 -0300

    Setup adjustments

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
 # get version from __version__ variable in ordem_servico/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
-with open('ordem_servico/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+with open("ordem_servico/__init__.py", "rb") as f:
+    version = str(
+        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
+    )
 
 setup(
-	name='ordem_servico',
-	version=version,
-	description='_',
-	author='laugusto',
-	author_email='laugusto@eucon.tech',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+    name="ordem_servico",
+    version=version,
+    description="_",
+    author="sleonardoaugusto",
+    author_email="sleonardoaugusto@gmail.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires],
 )
