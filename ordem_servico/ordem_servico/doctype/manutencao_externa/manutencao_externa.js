85f4a221378fa31fc798cc82ddaf9e5f5cd8b2d5
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-05-13 22:16:37 -0300

    Set mandatory field by javascript

--- ordem_servico/ordem_servico/doctype/manutencao_externa/manutencao_externa.js
+++ ordem_servico/ordem_servico/doctype/manutencao_externa/manutencao_externa.js
@@ -1,3 +1,11 @@
 // Copyright (c) 2019, laugusto and contributors
 // For license information, please see license.txt
 
+frappe.ui.form.on("Manutencao Externa", {
+    refresh: function(frm) {
+        if (frm.doc.docstatus == 0) {
+            frm.set_df_property("equip_problem", "reqd", 1);
+            frm.set_df_property("work_done", "reqd", 1);
+        }
+    }
+});
