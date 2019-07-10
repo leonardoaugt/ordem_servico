7af0771dcf5cb9317596f07a9e541810a24307b4
Leonardo <sleonardoaugusto@gmail.com>
2019-07-10 02:27:10 -0300

    Bug fix

--- ordem_servico/ordem_servico/doctype/manutencao_externa/manutencao_externa.js
+++ ordem_servico/ordem_servico/doctype/manutencao_externa/manutencao_externa.js
@@ -7,5 +7,16 @@ frappe.ui.form.on("Manutencao Externa", {
             frm.set_df_property("equip_problem", "reqd", 1);
             frm.set_df_property("work_done", "reqd", 1);
         }
+    },
+
+    /* Bug fix when trying to submit a Manutencao Externa
+     status was changing even validate fails */
+    before_submit(frm) {
+        if (
+            frm.doc.equip_problem == undefined
+            || frm.doc.work_done == undefined
+        ) {
+            frm.doc.workflow_state = 'Pendente';
+        }
     }
 });
