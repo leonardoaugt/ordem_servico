8e5259f8030702de55ec9150182be2a4dd70ebe5
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-04-13 00:08:22 -0300

    console log removed

--- ordem_servico/ordem_servico/doctype/ordem_servico_interna/ordem_servico_interna.js
+++ ordem_servico/ordem_servico/doctype/ordem_servico_interna/ordem_servico_interna.js
@@ -1,136 +1,138 @@
 // Copyright (c) 2018, laugusto and contributors
 // For license information, please see license.txt
 
-{% include "ordem_servico/public/js/ordem_servico.js" %}
+{%
+  include
+  'ordem_servico/public/js/ordem_servico.js' %
+}
 
 frappe.ui.form.on('Ordem Servico Interna', {
 
-	schedule_quotation_event: function (frm) {
-		
-		if (cur_frm.doc.__unsaved) {
-			frappe.throw('Favor salvar documento!');
-		}
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.make_event',
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-				start_date: frm.doc.quotation_schedule_date,
-				start_time: frm.doc.quotation_schedule_time,
-				work_time: frm.doc.quotation_time,
-				trigger: 'quotation',
-			},
-		});
-		frm.reload_doc();
-		show_alert('Orçamento agendado.');
-	},
+  schedule_quotation_event: function (frm) {
+
+    if (cur_frm.doc.__unsaved) {
+      frappe.throw('Favor salvar documento!')
+    }
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.make_event',
+      args: {
+        doctype: frm.doc.doctype,
+        docname: frm.doc.name,
+        start_date: frm.doc.quotation_schedule_date,
+        start_time: frm.doc.quotation_schedule_time,
+        work_time: frm.doc.quotation_time,
+        trigger: 'quotation'
+      }
+    })
+    frm.reload_doc()
+    show_alert('Orçamento agendado.')
+  },
 
 
-	start_quotation: function (frm) {
+  start_quotation: function (frm) {
 
-		if (cur_frm.doc.__unsaved) {
-			frappe.throw('Favor salvar documento!');
-		}
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.get_time_now',
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-				trigger: 'start_quotation',
-			},
-		});
-		show_alert('Orçamento iniciado.');
-		frm.reload_doc();
-	},
+    if (cur_frm.doc.__unsaved) {
+      frappe.throw('Favor salvar documento!')
+    }
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.get_time_now',
+      args: {
+        doctype: frm.doc.doctype,
+        docname: frm.doc.name,
+        trigger: 'start_quotation'
+      }
+    })
+    show_alert('Orçamento iniciado.')
+    frm.reload_doc()
+  },
 
-	end_quotation: function (frm) {
+  end_quotation: function (frm) {
 
-		if (cur_frm.doc.__unsaved) {
-			frappe.throw('Favor salvar documento!');
-		}
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.get_time_now',
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-				trigger: 'end_quotation',
-			},
-		});
-		show_alert('Orçamento finalizado.');
-		frm.reload_doc();
-	},
+    if (cur_frm.doc.__unsaved) {
+      frappe.throw('Favor salvar documento!')
+    }
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.get_time_now',
+      args: {
+        doctype: frm.doc.doctype,
+        docname: frm.doc.name,
+        trigger: 'end_quotation'
+      }
+    })
+    show_alert('Orçamento finalizado.')
+    frm.reload_doc()
+  },
 
-	create_quotation: function (frm) {
+  create_quotation: function (frm) {
 
-		if (frm.doc.__unsaved) {
-			frappe.throw(__("You have unsaved changes in this form. Please save before you continue."));
-		}
+    if (frm.doc.__unsaved) {
+      frappe.throw(__('You have unsaved changes in this form. Please save before you continue.'))
+    }
 
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.make_quotation',
-			args: {
-				os_docname: frm.doc.name,
-			},
-			callback: function(r) {
-				frappe.model.sync(r.message);
-				console.log(r.message);
-				frappe.set_route('Form', r.message.doctype, r.message.name);
-			}
-		});
-	},
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.make_quotation',
+      args: {
+        os_docname: frm.doc.name
+      },
+      callback: function (r) {
+        frappe.model.sync(r.message)
+        frappe.set_route('Form', r.message.doctype, r.message.name)
+      }
+    })
+  },
 
-	schedule_repair_event: function (frm) {
+  schedule_repair_event: function (frm) {
 
-		if (cur_frm.doc.__unsaved) {
-			frappe.throw('Favor salvar documento!');
-		}
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.make_event',
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-				start_date: frm.doc.repair_schedule_date,
-				start_time: frm.doc.repair_schedule_time,
-				work_time: frm.doc.repair_time,
-				trigger: 'repair',
-			},
-		});
-		frm.reload_doc();
-		show_alert('Conserto agendado.');
-	},
+    if (cur_frm.doc.__unsaved) {
+      frappe.throw('Favor salvar documento!')
+    }
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.make_event',
+      args: {
+        doctype: frm.doc.doctype,
+        docname: frm.doc.name,
+        start_date: frm.doc.repair_schedule_date,
+        start_time: frm.doc.repair_schedule_time,
+        work_time: frm.doc.repair_time,
+        trigger: 'repair'
+      }
+    })
+    frm.reload_doc()
+    show_alert('Conserto agendado.')
+  },
 
-	start_repair: function (frm) {
+  start_repair: function (frm) {
 
-		if (cur_frm.doc.__unsaved) {
-			frappe.throw('Favor salvar documento!');
-		}
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.get_time_now',
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-				trigger: 'start_repair',
-			},
-		});
-		frm.reload_doc();
-		show_alert('Conserto iniciado.');
-	},
+    if (cur_frm.doc.__unsaved) {
+      frappe.throw('Favor salvar documento!')
+    }
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.get_time_now',
+      args: {
+        doctype: frm.doc.doctype,
+        docname: frm.doc.name,
+        trigger: 'start_repair'
+      }
+    })
+    frm.reload_doc()
+    show_alert('Conserto iniciado.')
+  },
 
-	end_repair: function (frm) {
+  end_repair: function (frm) {
 
-		if (cur_frm.doc.__unsaved) {
-			frappe.throw('Favor salvar documento!');
-		}
-		frappe.call({
-			method: 'ordem_servico.ordem_servico.utils.get_time_now',
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-				trigger: 'end_repair',
-			},
-		});
-		frm.reload_doc();
-		show_alert('Conserto finalizado.');
-	},
+    if (cur_frm.doc.__unsaved) {
+      frappe.throw('Favor salvar documento!')
+    }
+    frappe.call({
+      method: 'ordem_servico.ordem_servico.utils.get_time_now',
+      args: {
+        doctype: frm.doc.doctype,
+        docname: frm.doc.name,
+        trigger: 'end_repair'
+      }
+    })
+    frm.reload_doc()
+    show_alert('Conserto finalizado.')
+  }
 
-});
+})
--- ordem_servico/public/js/payment_entry.js
+++ ordem_servico/public/js/payment_entry.js
@@ -1,36 +1,35 @@
 frappe.ui.form.on('Payment Entry', {
 
-    onload_post_render: function (frm) {
-        if (frm.doc.__islocal) {
-            // Get document previous route
-            prev_route = frappe.get_prev_route();
-            prev_doctype = prev_route[1];
-            prev_docname = prev_route[2];
-            prev_doc = frappe.get_doc(prev_doctype, prev_docname);
-            console.log(prev_doc);
+  onload_post_render: function (frm) {
+    if (frm.doc.__islocal) {
+      // Get document previous route
+      prev_route = frappe.get_prev_route()
+      prev_doctype = prev_route[1]
+      prev_docname = prev_route[2]
+      prev_doc = frappe.get_doc(prev_doctype, prev_docname)
 
-            // Check if is a Sales Order and has os_interna_link
-            if (prev_doc.doctype == 'Sales Order' || prev_doc.doctype == 'Quotation' && prev_doc.os_interna_link) {
-                frm.doc.os_interna_link = prev_doc.os_interna_link;
-                frm.doc.local_manutencao = prev_doc.local_manutencao;
-                refresh_field('os_interna_link');
-                refresh_field('local_manutencao');
-            }
-        }
-    },
-
-    after_save: function (frm) {
-        //Set Payment Entry on OS History section
-        if (frm.doc.os_interna_link) {
-            frappe.call({
-                method: 'ordem_servico.ordem_servico.utils.set_payment_entry_history',
-                args: {
-                    source_docname: frm.doc.name,
-                    source_transaction_date: frm.doc.posting_date,
-                    target_docname: frm.doc.os_interna_link,
-                }
-            });
-        }
+      // Check if is a Sales Order and has os_interna_link
+      if (prev_doc.doctype == 'Sales Order' || prev_doc.doctype == 'Quotation' && prev_doc.os_interna_link) {
+        frm.doc.os_interna_link = prev_doc.os_interna_link
+        frm.doc.local_manutencao = prev_doc.local_manutencao
+        refresh_field('os_interna_link')
+        refresh_field('local_manutencao')
+      }
     }
+  },
 
-});
+  after_save: function (frm) {
+    //Set Payment Entry on OS History section
+    if (frm.doc.os_interna_link) {
+      frappe.call({
+        method: 'ordem_servico.ordem_servico.utils.set_payment_entry_history',
+        args: {
+          source_docname: frm.doc.name,
+          source_transaction_date: frm.doc.posting_date,
+          target_docname: frm.doc.os_interna_link
+        }
+      })
+    }
+  }
+
+})
