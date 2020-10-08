03316247b54d7537879d7f5b96143e518ad657ac
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-10-08 01:04:22 -0300

    Refactor; Reload doc fix

--- ordem_servico/ordem_servico/doctype/ordem_servico_externa/ordem_servico_externa.js
+++ ordem_servico/ordem_servico/doctype/ordem_servico_externa/ordem_servico_externa.js
@@ -2,85 +2,66 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Ordem Servico Externa', {
-
-	refresh: function (frm) {
-		frm.events.apply_filters(frm);
-		frm.events.add_os_button(frm);
-		frm.events.add_schedule_button(frm);
-		frm.events.add_cancel_button(frm);
+	refresh(frm) {
+		frm.events.apply_filters(frm)
+		frm.events.add_os_button(frm)
+		frm.events.add_schedule_button(frm)
+		frm.events.add_cancel_button(frm)
 	},
-
-	add_os_button: function (frm) {
+	add_os_button(frm) {
 		if (frm.events.show_maint_button(frm)) {
 			frm.add_custom_button(__('Gerar Manutenção'),
 				() => frm.events.make_maintenance(frm),
-				__("Make"));
+				__("Make"))
 		}
 	},
-
-	add_schedule_button: function (frm) {
+	add_schedule_button(frm) {
 		if (frm.events.show_schedule_button(frm)) {
 			frm.add_custom_button(__('Agendar Manutenção'),
 				() => frm.events.schedule_maintenance(frm),
-				__("Make"));
+				__("Make"))
 		}
 	},
-
-	add_cancel_button: function (frm) {
+	add_cancel_button(frm) {
 		if (frm.events.show_cancel_button(frm)) {
 			frm.add_custom_button(__('Cancelar'),
 				() => frm.events.cancel_maintenances(frm),
-				__("Status"));
+				__("Status"))
 		}
 	},
-
-	show_maint_button: function (frm) {
-		let status = 'Enviado';
-		let valid = frm.doc.workflow_state == status ? true : false;
-
-		return valid;
+	show_maint_button(frm) {
+		return frm.doc.workflow_state === 'Enviado'
 	},
-
-	show_schedule_button: function (frm) {
-		let status = 'Agendamento Pendente';
-		let valid = frm.doc.workflow_state == status ? true : false;
-
-		return valid;
+	show_schedule_button(frm) {
+		return frm.doc.workflow_state === 'Agendamento Pendente'
 	},
-
-	show_cancel_button: function (frm) {
-		let status = 'Concluído';
-		let valid = frm.doc.workflow_state != status ? true : false;
-
-		return valid;
+	show_cancel_button(frm) {
+		return frm.doc.workflow_state !== 'Concluído'
 	},
-
-	make_maintenance: function (frm) {
+	make_maintenance(frm) {
 		frappe.call({
 			method: 'ordem_servico.ordem_servico.doctype.manutencao_externa.manutencao_externa.make_maintenance',
 			args: {
 				docname: frm.doc.name,
 			},
-			callback: function (r) {
+			callback() {
+				frm.reload_doc()
 				frappe.show_alert({
 					message: 'Manutenções geradas!',
 					indicator: 'green',
 				})
 			}
-
 		})
-		frm.reload_doc();
 	},
-
-	schedule_maintenance: function (frm) {
+	schedule_maintenance(frm) {
 		frappe.call({
 			method: 'ordem_servico.ordem_servico.doctype.ordem_servico_externa.ordem_servico_externa.schedule_maintenance',
 			args: {
 				docname: frm.doc.name,
 				repair_person: frm.doc.repair_person,
 			},
-			callback: function (r) {
-				frm.reload_doc();
+			callback() {
+				frm.reload_doc()
 				frappe.show_alert({
 					message: 'Manutenção agendada!',
 					indicator: 'green',
@@ -88,8 +69,7 @@ frappe.ui.form.on('Ordem Servico Externa', {
 			}
 		})
 	},
-
-	cancel_maintenances: function (frm) {
+	cancel_maintenances(frm) {
 		frappe.confirm(`Tem certeza que deseja cancelar a Ordem de Serviço ${frm.doc.name}?`,
 			function () {
 				frappe.call({
@@ -97,8 +77,8 @@ frappe.ui.form.on('Ordem Servico Externa', {
 					args: {
 						docname: frm.doc.name,
 					},
-					callback: function (r) {
-						frm.reload_doc();
+					callback(r) {
+						frm.reload_doc()
 						frappe.show_alert({
 							message: 'Manutenções canceladas!',
 							indicator: 'grey',
@@ -107,18 +87,17 @@ frappe.ui.form.on('Ordem Servico Externa', {
 				})
 			},
 			function () {
-				window.close();
+				window.close()
 			}
 		)
 	},
-
-	apply_filters (frm) {
+	apply_filters(frm) {
 		frm.fields_dict['os_equipments'].grid.get_field('serie').get_query = () => {
 			return {
 				filters: {
 					'parent': frm.doc.customer,
 				}
 			}
-		};
+		}
 	}
-});
+})
